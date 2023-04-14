070fc2c0f7edd50f3e6b3b611a85e53518f29994
HuntDubs <Hunterwilson4092@gmail.com>
2023-04-14 14:53:17 -0500

    fixed invalid syntax

--- utils/utils/general.py
+++ utils/utils/general.py
@@ -58,4 +58,4 @@ def make_divisible(x, divisor):
     # Returns nearest x divisible by divisor
     if isinstance(divisor, torch.Tensor):
         divisor = int(divisor.max())  # to int
-    return math.ceil(x / divisor) * 
+    return math.ceil(x / divisor) * divisor
