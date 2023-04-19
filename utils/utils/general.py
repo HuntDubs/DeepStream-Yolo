f55f6409ca9c8fc31a1478c7203bb2b24b5d7fb4
HuntDubs <Hunterwilson4092@gmail.com>
2023-04-18 19:44:00 -0500

    Added anchor function

--- utils/utils/general.py
+++ utils/utils/general.py
@@ -123,3 +123,12 @@ def make_divisible(x, divisor):
         divisor = int(divisor.max())  # to int
     return math.ceil(x / divisor) * divisor
 
+def check_anchor_order(m):
+    # Check anchor order against stride order for YOLOv5 Detect() module m, and correct if necessary
+    a = m.anchors.prod(-1).mean(-1).view(-1)  # mean anchor area per output layer
+    da = a[-1] - a[0]  # delta a
+    ds = m.stride[-1] - m.stride[0]  # delta s
+    if da and (da.sign() != ds.sign()):  # same order
+        LOGGER.info(f'Reversing anchor order')
+        m.anchors[:] = m.anchors.flip(0)
+
