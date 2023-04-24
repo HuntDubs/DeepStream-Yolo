9ade195beeeee79d223a773aef495472fec51a42
HuntDubs <Hunterwilson4092@gmail.com>
2023-04-24 15:44:14 -0500

    Fixed include

--- gen_wts_utils/yolov8/gen_wts_yoloV8.py
+++ gen_wts_utils/yolov8/gen_wts_yoloV8.py
@@ -2,8 +2,8 @@ import argparse
 import os
 import struct
 import torch
-from utils.torch_utils import select_device
-from utils.tal import make_anchors
+from ultralytics.utils.torch_utils import select_device
+from ultralytics.utils.tal import make_anchors
 
 
 class Layers(object):
