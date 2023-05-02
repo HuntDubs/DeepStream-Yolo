8f03d029d19340283cf71dadaabd2e3fb096ae3c
HuntDubs <Hunterwilson4092@gmail.com>
2023-05-02 11:45:54 -0500

    testing importance

--- gen_wts_utils/yolov8/ultralytics/nn/modules.py
+++ gen_wts_utils/yolov8/ultralytics/nn/modules.py
@@ -1,11 +1,11 @@
 import math
-
-import torch
+import warnings
 import torch.nn as nn
+import torch
 
 
 def autopad(k, p=None, d=1):  # kernel, padding, dilation
-    """Pad to 'same' shape outputs."""
+    # Pad to 'same' shape outputs.
     if d > 1:
         k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
     if p is None:
