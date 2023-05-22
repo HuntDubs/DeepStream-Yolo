c7ee76b7742ba61efaaea7dee395e6a3adad0437
HuntDubs <Hunterwilson4092@gmail.com>
2023-05-22 16:44:16 -0500

    Added util functions and class definitions

---
+++ gen_wts_utils/v6/yolov6/utils/events.py
@@ -0,0 +1,9 @@
+import logging
+
+def set_logging(name=None):
+    rank = int(os.getenv('RANK', -1))
+    logging.basicConfig(format="%(message)s", level=logging.INFO if (rank in (-1, 0)) else logging.WARNING)
+    return logging.getLogger(name)
+
+
+LOGGER = set_logging(__name__)
---
+++ gen_wts_utils/v6/yolov6/utils/torch_utils.py
@@ -0,0 +1,12 @@
+import torch.nn as nn
+
+def initialize_weights(model):
+    for m in model.modules():
+        t = type(m)
+        if t is nn.Conv2d:
+            pass
+        elif t is nn.BatchNorm2d:
+            m.eps = 1e-3
+            m.momentum = 0.03
+        elif t in [nn.Hardswish, nn.LeakyReLU, nn.ReLU, nn.ReLU6, nn.SiLU]:
+            m.inplace = True
