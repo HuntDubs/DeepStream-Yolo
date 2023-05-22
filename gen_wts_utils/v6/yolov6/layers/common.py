d39e95905db47ac09265c9f543d5eb9f47ec9a52
HuntDubs <Hunterwilson4092@gmail.com>
2023-05-22 18:12:30 -0500

    Updated import

--- gen_wts_utils/v6/yolov6/layers/common.py
+++ gen_wts_utils/v6/yolov6/layers/common.py
@@ -6,7 +6,7 @@ import torch
 import torch.nn as nn
 import torch.nn.init as init
 from torch.nn.parameter import Parameter
-from utils.torch_utils import download_ckpt
+from yolov6.utils.torch_utils import download_ckpt
 
 
 class SiLU(nn.Module):
