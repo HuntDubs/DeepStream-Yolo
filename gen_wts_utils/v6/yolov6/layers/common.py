8e66eec995c96181a2517c170a8ce8009fdfacb6
HuntDubs <Hunterwilson4092@gmail.com>
2023-05-22 20:42:50 -0500

    akd

--- gen_wts_utils/v6/yolov6/layers/common.py
+++ gen_wts_utils/v6/yolov6/layers/common.py
@@ -262,11 +262,11 @@ class RepVGGBlock(nn.Module):
 
     #     return self.nonlinearity(self.se(self.rbr_dense(inputs) + self.rbr_1x1(inputs) + id_out))
 
-    # def get_equivalent_kernel_bias(self):
-    #     kernel3x3, bias3x3 = self._fuse_bn_tensor(self.rbr_dense)
-    #     kernel1x1, bias1x1 = self._fuse_bn_tensor(self.rbr_1x1)
-    #     kernelid, biasid = self._fuse_bn_tensor(self.rbr_identity)
-    #     return kernel3x3 + self._pad_1x1_to_3x3_tensor(kernel1x1) + kernelid, bias3x3 + bias1x1 + biasid
+    def get_equivalent_kernel_bias(self):
+        kernel3x3, bias3x3 = self._fuse_bn_tensor(self.rbr_dense)
+        kernel1x1, bias1x1 = self._fuse_bn_tensor(self.rbr_1x1)
+        kernelid, biasid = self._fuse_bn_tensor(self.rbr_identity)
+        return kernel3x3 + self._pad_1x1_to_3x3_tensor(kernel1x1) + kernelid, bias3x3 + bias1x1 + biasid
 
     # def _avg_to_3x3_tensor(self, avgp):
     #     channels = self.in_channels
