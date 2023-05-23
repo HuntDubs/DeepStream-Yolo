1f4158ce29cae493a2e8594e4e6b1442469b74a1
HuntDubs <Hunterwilson4092@gmail.com>
2023-05-22 20:44:35 -0500

    Uncomment functionality

--- gen_wts_utils/v6/yolov6/layers/common.py
+++ gen_wts_utils/v6/yolov6/layers/common.py
@@ -283,33 +283,33 @@ class RepVGGBlock(nn.Module):
     #     else:
     #         return torch.nn.functional.pad(kernel1x1, [1, 1, 1, 1])
 
-    # def _fuse_bn_tensor(self, branch):
-    #     if branch is None:
-    #         return 0, 0
-    #     if isinstance(branch, nn.Sequential):
-    #         kernel = branch.conv.weight
-    #         running_mean = branch.bn.running_mean
-    #         running_var = branch.bn.running_var
-    #         gamma = branch.bn.weight
-    #         beta = branch.bn.bias
-    #         eps = branch.bn.eps
-    #     else:
-    #         assert isinstance(branch, nn.BatchNorm2d)
-    #         if not hasattr(self, 'id_tensor'):
-    #             input_dim = self.in_channels // self.groups
-    #             kernel_value = np.zeros((self.in_channels, input_dim, 3, 3), dtype=np.float32)
-    #             for i in range(self.in_channels):
-    #                 kernel_value[i, i % input_dim, 1, 1] = 1
-    #             self.id_tensor = torch.from_numpy(kernel_value).to(branch.weight.device)
-    #         kernel = self.id_tensor
-    #         running_mean = branch.running_mean
-    #         running_var = branch.running_var
-    #         gamma = branch.weight
-    #         beta = branch.bias
-    #         eps = branch.eps
-    #     std = (running_var + eps).sqrt()
-    #     t = (gamma / std).reshape(-1, 1, 1, 1)
-    #     return kernel * t, beta - running_mean * gamma / std
+    def _fuse_bn_tensor(self, branch):
+        if branch is None:
+            return 0, 0
+        if isinstance(branch, nn.Sequential):
+            kernel = branch.conv.weight
+            running_mean = branch.bn.running_mean
+            running_var = branch.bn.running_var
+            gamma = branch.bn.weight
+            beta = branch.bn.bias
+            eps = branch.bn.eps
+        else:
+            assert isinstance(branch, nn.BatchNorm2d)
+            if not hasattr(self, 'id_tensor'):
+                input_dim = self.in_channels // self.groups
+                kernel_value = np.zeros((self.in_channels, input_dim, 3, 3), dtype=np.float32)
+                for i in range(self.in_channels):
+                    kernel_value[i, i % input_dim, 1, 1] = 1
+                self.id_tensor = torch.from_numpy(kernel_value).to(branch.weight.device)
+            kernel = self.id_tensor
+            running_mean = branch.running_mean
+            running_var = branch.running_var
+            gamma = branch.weight
+            beta = branch.bias
+            eps = branch.eps
+        std = (running_var + eps).sqrt()
+        t = (gamma / std).reshape(-1, 1, 1, 1)
+        return kernel * t, beta - running_mean * gamma / std
 
     def switch_to_deploy(self):
         if hasattr(self, 'rbr_reparam'):
